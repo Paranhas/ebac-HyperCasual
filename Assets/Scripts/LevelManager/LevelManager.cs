e5a31f611af17a2860e5b61d13ef4fa99ac66c7f
Paranhas <rodrigoparanhas.br@gmail.com>
2025-10-21 11:53:45 -0300

    Update LevelManager.cs

--- Assets/Scripts/LevelManager/LevelManager.cs
+++ Assets/Scripts/LevelManager/LevelManager.cs
@@ -12,15 +12,6 @@ public class LevelManager : MonoBehaviour
     private List<LevelPieceBase> _spawnedPieces = new List<LevelPieceBase>();
     private LevelPieceBasedSetup _currSetup;
 
-    /*[Header("Pieces")]
-    public List<LevelPieceBase> levelStartPieces;
-    public List<LevelPieceBase> levelPieces;
-    public List<LevelPieceBase> levelEndPieces;
-
-    public int pieceStartNumber = 3;
-    public int pieceNumber = 5;
-    public int pieceEndNumber = 1;*/
-
     public void Awake()
     {
        // SpawnNextLevel();
